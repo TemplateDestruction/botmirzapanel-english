d7415db590e932a015c960e87a6f0296b7684316
Trulyinfinite <40712315+TrulyInfinite@users.noreply.github.com>
2025-02-16 01:21:10 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -249,13 +249,13 @@ function install_bot() {
     fi
 
     # Default to latest release
-    ZIP_URL=$(curl -s https://api.github.com/repos/mahdiMGF2/botmirzapanel/releases/latest | grep "zipball_url" | cut -d '"' -f 4)
+    ZIP_URL=$(curl -s https://api.github.com/repos/TrulyInfinite/botmirzapanel-english/releases/latest | grep "zipball_url" | cut -d '"' -f 4)
 
 # Check for version flag
 if [[ "$1" == "-v" && "$2" == "beta" ]] || [[ "$1" == "-beta" ]] || [[ "$1" == "-" && "$2" == "beta" ]]; then
-    ZIP_URL="https://github.com/mahdiMGF2/botmirzapanel/archive/refs/heads/main.zip"
+    ZIP_URL="https://github.com/TrulyInfinite/botmirzapanel-english/archive/refs/heads/main.zip"
 elif [[ "$1" == "-v" && -n "$2" ]]; then
-    ZIP_URL="https://github.com/mahdiMGF2/botmirzapanel/archive/refs/tags/$2.zip"
+    ZIP_URL="https://github.com/TrulyInfinite/botmirzapanel-english/archive/refs/tags/$2.zip"
 fi
 
     # Download and extract the repository
